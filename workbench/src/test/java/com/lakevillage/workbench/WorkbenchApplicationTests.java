3f47e71be7368c28148a55c7df02a7601fc5679a
changmin.lee <changmanyi@gmail.com>
2023-07-14 22:54:12 +0900

    initial commit

---
+++ workbench/src/test/java/com/lakevillage/workbench/WorkbenchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakevillage.workbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbenchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
